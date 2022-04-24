ae50b58a3ee8f6ac10930f71ba1955294ac1464b
VadymDenysiuk <80970410371q@gmail.com>
2022-04-24 17:24:28 +0300

    add new methods and files

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const { getCurrent } = require('../../controllers/users/getCurrent')
 const { auth } = require('../../middlewares/auth')
+const { updateAvatar } = require('../../controllers/users/updateAvatar')
+const { upload } = require('../../middlewares/upload')
 
 const router = express.Router();
 
 router.get('/current', auth, getCurrent)
+
+router.patch('/avatars', auth, upload.single('avatar'), updateAvatar)
 
 module.exports = router
 
